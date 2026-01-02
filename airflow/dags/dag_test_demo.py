3675b9619a221267a03cf921df9709a848c032de
etlavenkateshwarlu-dev <etla.venkateshwarlu@gmail.com>
2026-01-02 19:57:19 +0530

    smaple dag file

---
+++ airflow/dags/dag_test_demo.py
@@ -0,0 +1,45 @@
+from datetime import datetime,timedelta
+from datetime import date
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.dummy import DummyOperator
+
+email_list=['vte558@gmail.com','etla.venkateshwarlu@gmail.com']
+default_arguments={
+    'depends_on_past': False,
+    'email_on_failure': True,
+    'email_on_retry': False,
+    'email': email_list,
+    'retries': 3,
+    'retry_delay': timedelta(minutes=5)
+}
+
+dag = DAG(
+    'My_first_airflow_application',
+    description='Test Dag Demo',
+    schedule_interval=timedelta(days=5),
+    default_args=default_arguments,
+    start_date=datetime(2025, 1, 11),
+    catchup=False,
+    tags=['DEV']
+)
+start_task = DummyOperator(
+    task_id='start_Task',
+    dag=dag
+)
+bash_task1 = BashOperator(
+    task_id='echo_task1',
+    bash_command='echo Hello Welcome to Airflow',
+    dag=dag
+)
+bash_task2 = BashOperator(
+    task_id='echo_task2',
+    bash_command='echo Hi How are you',
+    dag=dag
+)
+
+end_task = DummyOperator(
+    task_id='End_Task'
+)
+
+start_task >> bash_task1 >> bash_task2 >> end_task
